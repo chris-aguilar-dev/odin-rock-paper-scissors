2dcb741375ce8781579a913cc72e92dd92c80a97
clutzz <chris.aguilar.dev@gmail.com>
2023-09-04 15:27:38 -0500

    Final game

--- script.js
+++ script.js
@@ -1,87 +1,107 @@
-//A way to start the game
-function playGame() {
-    //Each player needs a selection
-    const playerSelection = getPlayerChoice();
-    const computerSelction = getComputerChoice();
+//Dom variables
+const rockButton = document.getElementById('rock-button');
+const paperButton = document.getElementById('paper-button');
+const scissorsButton = document.getElementById('scissors-button');
+const resultPara = document.getElementById('result-p');
+const humanScoreSpan = document.getElementById('human-score');
+const computerScoreSpan = document.getElementById('computer-score');
+const winnerPointsSpan = document.getElementById('winner-points');
 
-    if (playerSelection == 'rock') {
-        if (computerSelction == 'rock') {
-            return "It's a tie";
-        } else if (computerSelction == 'paper') {
-            return "You lose";
-        } else {
-            return "You win";
-        }
-    } else if (playerSelection == 'paper') {
-        if (computerSelction == 'rock') {
-            return "You win";
-        } else if (computerSelction == 'paper') {
-            return "It's a tie";
-        } else {
-            return "You lose"
-        }
-    } else if (playerSelection == 'scissors') {
-        if (computerSelction == 'rock') {
-            return "You lose";
-        } else if (computerSelction == 'paper') {
-            return "You win";
-        } else {
-            return "It's a tie"
-        }
+//Keep track of scores
+let humScore = 0;
+let compScore = 0; 
+
+//How many points will the winner have
+const winnerPoints = 7;
+
+//function that gives computer result
+function computerResult() {
+    let result = Math.floor(Math.random() * 3);
+    if (result === 0) {
+        return 'rock';
+    } else if (result === 1) {
+        return 'paper';
     } else {
-        return "Please make a valid selection."
-    }
-}
-
-//Function to get player choice
-function getPlayerChoice() {
-    let playerChoice = prompt("Choose rock, paper, or scissors.");
-    let answer = playerChoice.toLowerCase();
-    return answer;
-}
-
-//Function to get computer choice
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice == 0) {
-        return 'rock'
-    } else if (computerChoice == 1) {
-        return 'paper'
-    } else {
-        return 'scissors'
-    }
-}
-
-//Function that runs the game any number of times
-function autoGame(num) {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < num; i++) {
-        let game = playGame();
-
-        if (game == 'You win') {
-            playerScore++;
-            alert(`You win; Human: ${playerScore}, Computer: ${computerScore}`);
-        } else if (game == 'You lose') {
-            computerScore++;
-            alert(`You lose; Human: ${playerScore}, Computer: ${computerScore}`);
-        } else {
-            alert(`It's a tie; Human: ${playerScore}, Computer: ${computerScore}`);
-        }
-
-        console.log(game);
-        
-    }
-
-    if (playerScore > computerScore) {
-        alert("Final Result: Win")
-        console.log("Final Result: Win")
-    } else if (playerScore < computerScore) {
-        alert("Final Result: Lost")
-        console.log("Final Result: Lost")
-    } else {
-        alert("Final Result: Tie")
-        console.log("Final Result: Tie")
+        return 'scissors';
     }
 };
+
+//function that gives result to a game
+function gameResult(userChoice) {
+    const compChoice = computerResult();
+    const WINNERMESSAGE = `Computer chose ${compChoice}, You win!`;
+    const LOSERMESSAGE = `Computer chose ${compChoice}, You lose!`;
+    const TIEMESSAGE = `Computer chose ${compChoice}, It's a tie!`;
+    
+
+    if (humScore === winnerPoints || compScore === winnerPoints) {
+        return;
+    } else {
+        if (userChoice === 'rock') {
+            if (compChoice === 'rock') {
+                resultPara.textContent = TIEMESSAGE;
+                humanScoreSpan.textContent = humScore;
+                computerScoreSpan.textContent = compScore;
+            } else if (compChoice === 'paper') {
+                resultPara.textContent = LOSERMESSAGE;
+                compScore++;
+                humanScoreSpan.textContent = humScore;
+                computerScoreSpan.textContent = compScore;
+            } else if (compChoice === 'scissors') {
+                resultPara.textContent = WINNERMESSAGE;
+                humScore++;
+                humanScoreSpan.textContent = humScore;
+                computerScoreSpan.textContent = compScore;
+            }
+        } else if (userChoice === 'paper') {
+            if (compChoice === 'rock') {
+                resultPara.textContent = WINNERMESSAGE;
+                humScore++;
+                humanScoreSpan.textContent = humScore;
+                computerScoreSpan.textContent = compScore;
+            } else if (compChoice === 'paper') {
+                resultPara.textContent = TIEMESSAGE;
+                humanScoreSpan.textContent = humScore;
+                computerScoreSpan.textContent = compScore;
+            } else if (compChoice === 'scissors') {
+                resultPara.textContent = LOSERMESSAGE;
+                compScore++;
+                humanScoreSpan.textContent = humScore;
+                computerScoreSpan.textContent = compScore;
+            }
+        } else {
+            if (compChoice === 'rock') {
+                resultPara.textContent = LOSERMESSAGE;
+                compScore++;
+                humanScoreSpan.textContent = humScore;
+                computerScoreSpan.textContent = compScore;
+            } else if (compChoice === 'paper') {
+                resultPara.textContent = WINNERMESSAGE;
+                humScore++;
+                humanScoreSpan.textContent = humScore;
+                computerScoreSpan.textContent = compScore;
+            } else if (compChoice === 'scissors') {
+                resultPara.textContent = TIEMESSAGE;
+                humanScoreSpan.textContent = humScore;
+                computerScoreSpan.textContent = compScore;
+            }
+        }
+    }
+
+    if (humScore === winnerPoints) {
+        resultPara.textContent = "Congrats You Won!"
+    } else if (compScore === winnerPoints) {
+        resultPara.textContent = "Sorry You Lost!"
+    }
+};
+
+//function that plays the game
+function playGame(e) {
+    gameResult(e.target.value);
+};
+
+winnerPointsSpan.textContent = winnerPoints;
+
+rockButton.addEventListener('click', playGame);
+paperButton.addEventListener('click', playGame);
+scissorsButton.addEventListener('click', playGame)
